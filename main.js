124fdaff1d570ddb6a7c9915d8bfb2de9c550754
Enes Osmanoğlu <enesosmanoglu@gmail.com>
2022-09-29 04:16:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ import { Player } from "./lib/Player.js";
 let player = new Player();
 
 /**@param {{time:number,canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D}} */
-export function start({ time, canvas, ctx }) {
-    player.start({ time, canvas, ctx });
+export function start({ time, canvas, ctx, dt }) {
+    player.start({ time, canvas, ctx, dt });
 }
 
 /**@param {{time:number,canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D}} */
-export function update({ time, canvas, ctx }) {
-    player.update({ time, canvas, ctx });
+export function update({ time, canvas, ctx, dt }) {
+    player.update({ time, canvas, ctx, dt });
 }
 
 /**@param {{time:number,canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D}} */
-export function draw({ time, canvas, ctx }) {
-    player.draw({ time, canvas, ctx });
+export function draw({ time, canvas, ctx, dt }) {
+    player.draw({ time, canvas, ctx, dt });
 }
